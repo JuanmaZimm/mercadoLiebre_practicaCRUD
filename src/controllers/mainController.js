2a225917e6b7b1765d94d5cd89449ed39ff37f5d
JuanmaZimm <juanmanuelzimmermann@gmail.com>
2022-10-31 23:31:50 -0300

    primer commit

---
+++ src/controllers/mainController.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		let visitedProducts = products.filter(product => product.category == 'visited');
+		let insaleProducts = products.filter(product => product.category == 'in-sale');
+		res.render('index', {sales: insaleProducts, visited: visitedProducts})
+	},
+	search: (req, res) => {
+		// Do the magic
+	},
+};
+
+module.exports = controller;
